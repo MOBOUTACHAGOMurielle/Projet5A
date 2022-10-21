797780daf44efd2850e1f3fb98c92290fe48c43c
MOBOUTACHAGOMurielle <78790775+MOBOUTACHAGOMurielle@users.noreply.github.com>
2022-10-21 09:21:58 +0200

    pageAccueilFront

---
+++ FrontProjet5A/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ apishop/src/main/java/com/polytech/apishop/ApishopApplication.java
@@ -0,0 +1,13 @@
+package com.polytech.apishop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApishopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApishopApplication.class, args);
+	}
+
+}
