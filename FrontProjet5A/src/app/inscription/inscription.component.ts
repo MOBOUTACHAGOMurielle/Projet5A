0b68879d4af9ea6b4af8e6496bc4f795facb3635
HDADI Achraf <achrafhdadi@hotmail.com>
2022-10-28 13:44:08 +0200

    requis

--- FrontProjet5A/src/app/inscription/inscription.component.ts
+++ FrontProjet5A/src/app/inscription/inscription.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-inscription',
   templateUrl: './inscription.component.html',
   styleUrls: ['./inscription.component.css']
 })
 export class InscriptionComponent implements OnInit {
+  form: any ={
+    email: null,
+    password: null,
+  }
+  constructor(
 
-  constructor() { }
+  ) { }
 
   ngOnInit(): void {
   }
-
+  onSubmit(): void{
+    console.log(this.form)
+    
+  }
+ 
 }
